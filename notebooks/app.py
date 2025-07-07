82220dfba29b0e16d315bbf7c5b25cecf55c1de7
josegrb <josegrbusiness@gmail.com>
2025-07-07 18:42:37 -0300

    Análise exploratorio

---
+++ notebooks/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('Análise de Veículos')
+
+car_data = pd.read_csv('../vehicles.csv')
+
+st.header('Análise Exploratória')
+
+if st.button('Criar Histograma da Quilometragem'):
+    st.write('Histograma gerado:')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig)
+
+
+
+if st.button('Criar Gráfico de Dispersão (Quilometragem vs Preço)'):
+    st.write('Gráfico de dispersão gerado:')
+    fig2 = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig2)
+
